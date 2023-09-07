594d93a4a7231ad6b28450a09f3a6d9b1c214632
hansel.bonifacio <hanselbonifaciot@gmail.com>
2023-09-07 12:28:40 -0500

    mejora en visualizacion historial

--- src/app/admin/pages/admin-historial-remision-page/admin-historial-remision-page.component.ts
+++ src/app/admin/pages/admin-historial-remision-page/admin-historial-remision-page.component.ts
@@ -1,60 +1,75 @@
-import { Component , OnInit} from '@angular/core';
-import { ActivatedRoute,Router } from '@angular/router';
-import {HistorialRemision} from '../../interfaces/historialRemison.interface';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HistorialRemision } from '../../interfaces/historialRemison.interface';
 import { AdminRemisionService } from '../../services/admin-remision.service';
 import { MaestrosService } from '../../../shared/services/maestros/maestros.service';
-import { EstadoCita,getNombreEstadoCitaById } from 'src/app/shared/interfaces/maestros.interfaces';
-
+import { EstadoCita, getNombreEstadoCitaById } from 'src/app/shared/interfaces/maestros.interfaces';
+import { Cita } from '../../interfaces/historialRemison.interface';
 
 @Component({
   selector: 'app-admin-historial-remision-page',
   templateUrl: './admin-historial-remision-page.component.html',
   styleUrls: ['./admin-historial-remision-page.component.css']
 })
-export class AdminHistorialRemisionPageComponent implements OnInit{
+export class AdminHistorialRemisionPageComponent implements OnInit {
 
   idRemision: string = "";
 
-  currentPageRegistro : number  = 1;
+  currentPageRegistro: number = 1;
 
-  currentPageHistorico : number  = 1;
+  currentPageHistorico: number = 1;
   maxCeldadRegistros = 3;
 
-  remisionDataActual  : HistorialRemision|any={};
-  historialRemisiones : HistorialRemision[] = [];
-  estados:EstadoCita[]=[];
-  constructor (
-    private adminService:AdminRemisionService,
+  remisionDataActual: HistorialRemision | any = {};
+  historialRemisiones: HistorialRemision[] = [];
+  estados: EstadoCita[] = [];
+  constructor(
+    private adminService: AdminRemisionService,
     private maestroService: MaestrosService,
-    private activateRoute : ActivatedRoute,
+    private activateRoute: ActivatedRoute,
     private router: Router
-    ){}
+  ) { }
 
   ngOnInit() {
     this.maestroService.getEstadosCita()
-    .subscribe(resp=>{
-      this.estados=resp
-    });
+      .subscribe(resp => {
+        this.estados = resp
+      });
 
     this.activateRoute.params.subscribe(
       params => {
-        this.idRemision = params ['idRemision'];
+        this.idRemision = params['idRemision'];
       }
     )
     this.adminService.consultarHistorialRemision(this.idRemision)
-    .subscribe(resp =>{
-      this.historialRemisiones = resp
-    })
+      .subscribe(resp => {
+        this.historialRemisiones = resp
+      })
     this.adminService.consultarDataActualRemision(this.idRemision)
-    .subscribe(resp =>{
-      this.remisionDataActual = resp
-    })
+      .subscribe(resp => {
+        this.remisionDataActual = resp
+      })
   }
 
-  getNombreEstadoCita(id:string){
-    return getNombreEstadoCitaById(id,this.estados);
+  getNombreEstadoCita(id: string) {
+    return getNombreEstadoCitaById(id, this.estados);
   }
-  backRemisionesTabla(){
+  backRemisionesTabla() {
     this.router.navigate(['admin/remisiones']);
   }
+
+  agregarCitasEstadoAnterior(citaAnteriores: Cita[], citasNuevas: Cita[], fechaAplicacionNovedad: Date): Cita[] {
+    let cn: Cita[] = citasNuevas?.map(cita => ({ ...cita, datosCita: { ...cita.datosCita, idEstado: "0" } })) ?? [];
+    cn = cn.filter(c => fechaAplicacionNovedad < c.datosCita.fechaProgramada);
+    if (citaAnteriores != null) {
+      const ca: Cita[] = citaAnteriores.slice();
+
+      return ca.concat(cn);
+
+    } else {
+      //cn = cn.filter(c1 => !this.remisionDataActual.citas.some((c2: { datosCita: { idCita: string; }; }) => c2.datosCita.idCita === c1.datosCita.idCita));
+      return cn;
+    }
+  }
+
 }

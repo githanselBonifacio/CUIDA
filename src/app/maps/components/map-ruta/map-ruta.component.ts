9023572b2cbc10d424fd136e99a5b61a823681c2
hansel.bonifacio <hanselbonifaciot@gmail.com>
2023-12-29 18:31:57 -0500

    configuracion de  map ruta

--- src/app/maps/components/map-ruta/map-ruta.component.ts
+++ src/app/maps/components/map-ruta/map-ruta.component.ts
@@ -1,8 +1,6 @@
 import { Component, Inject, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { Tarea } from 'src/app/diagramas/interfaces/tarea-gantt.interface';
-import { DatePipe } from '@angular/common';
 import { formatoHora } from 'src/app/shared/interfaces/maestros/maestros.interfaces';
 (mapboxgl as any).accessToken = 'pk.eyJ1IjoiaGFuc2Vib24iLCJhIjoiY2xqZGwwcm11MDF1MjNlazZlcDMzN3p3cCJ9.unVEZ1BzsP-9wdIdGqRK7A';
 
@@ -24,8 +22,8 @@ export class MapRutaComponent implements AfterViewInit {
   ngAfterViewInit(): void {
 
     let map = new mapboxgl.Map({
-      container: this.divMap?.nativeElement, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      container: this.divMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
       center: [this.data["origen"].longitud, this.data["origen"].latitud],
       zoom: 15,
 
@@ -42,7 +40,7 @@ export class MapRutaComponent implements AfterViewInit {
       if (tarea.tipo != "DVISITA") {
         const markerHtml = document.createElement("div");
         markerHtml.innerHTML = `
-        <div class="marker" style="transform: translate(-50%,-50%); display: flex;flex-direction: column;justify-content: center;clip-path: polygon(0% 0%, 100% 0%, 100% 90%, 55% 90%, 50% 100%, 45% 90%, 0% 90%);"> 
+        <div class="marker" style=" display: flex;flex-direction: column;justify-content: center;clip-path: polygon(0% 0%, 100% 0%, 100% 90%, 55% 90%, 50% 100%, 45% 90%, 0% 90%);"> 
             <div class="hora" style=" font-size:0.8rem; background-color: gray;color: white;padding: 0.5rem;border-radius: 5px;text-align: center;">
               <div>${tarea.id}</div>
               <div>${formatoHora(new Date(tarea.fechaProgramada))}</div>
